ccc756aa4d1c9b86c3c13ff53cacdc85a50faad4
MohamedTitoo0 <mo7371100@gmail.com>
2023-05-22 11:32:49 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:faaacdetector/models/learinghome.dart';
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp(LearningApp());
+}
+
+class LearningApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryTextTheme: TextTheme(headline6: TextStyle(color: Colors.white)),
+      ),
+      home: LearningHome(),
+    );
+  }
+}
+
